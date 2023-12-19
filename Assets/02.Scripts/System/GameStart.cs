957a8bd58ee4adb57c9d8f98e9e0451d3e654c7c
dasoya <66422476+dasoya@users.noreply.github.com>
2023-12-19 13:10:18 +0900

    add start scene

---
+++ Assets/02.Scripts/System/GameStart.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStart : MonoBehaviour
+{
+    public InputField baseInput;
+
+    public InputField nameInput;
+
+    private Button startBtn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startBtn = gameObject.GetComponent<Button>();
+        
+        
+    //시간 재는거 코드
+    
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void gameStart()
+    {
+        
+    }
+}
