2a163d01f224eec86812c01c5fdf3c4bf4c02449
dasoya <66422476+dasoya@users.noreply.github.com>
2024-01-02 20:27:34 +0900

    change server URL

--- Assets/02.Scripts/_JW/JSON/GameURL.cs
+++ Assets/02.Scripts/_JW/JSON/GameURL.cs
@@ -6,8 +6,8 @@ public class GameURL : MonoBehaviour
 {
     public static class NPCServer
     {
-       // public static readonly string Server_URL = "http://3.135.8.77:8000/";
-        public static readonly string Server_URL = "http://127.0.0.1:8000/";
+        public static readonly string Server_URL = "http://52.14.83.66:8000/";
+        //public static readonly string Server_URL = "http://127.0.0.1:8000/";
     
         public static readonly string getServerTime = "servertime/";
         
