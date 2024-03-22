4953797c5097ef6c3972ca01a0ed6645c5169e5a
dasoya <66422476+dasoya@users.noreply.github.com>
2024-03-22 15:52:12 +0900

    Fix User Chat UI

--- Assets/02.Scripts/_JW/DIALOGUE/SendMessage.cs
+++ Assets/02.Scripts/_JW/DIALOGUE/SendMessage.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,7 @@ using TMPro;
 public class SendMessage : MonoBehaviour
 {
     // chatting transfer limit
-    const int maxTransferNum = 7;
+    const int maxTransferNum = 4; // TODO : 바꿔야함
     private int transferNum = 0; // Number of transfers
 
     // chatting UI
@@ -15,12 +16,20 @@ public class SendMessage : MonoBehaviour
     public RectTransform ContentRect;
     public Scrollbar scrollBar;
     public TMP_InputField inputField;
-
+    private UnityEngine.UI.Button sendBtn;
     public string targetPersonaName = "이자식";
+    
+    
 
     private bool TestMode = false;
+
+    private void Awake()
+    {
+        sendBtn = GetComponent<UnityEngine.UI.Button>();
+    }
+
     public void Transfer(){
-        if(transferNum <= 7){
+        if(transferNum < maxTransferNum){
             
             if(inputField.text == "") return;
 
@@ -29,14 +38,8 @@ public class SendMessage : MonoBehaviour
             StartCoroutine(showNPCMessageCoroutine());
            
             transferNum++;
-         
-        } else{
             
-            //종료창 띄워야함.
-            inputField.interactable = true;
-            // cannot send message
-            Debug.Log("더 이상 메세지를 전송할 수 없습니다.");
-        }
+        } 
     }
     
     private void PCMessage(){
@@ -47,8 +50,11 @@ public class SendMessage : MonoBehaviour
             
             Area.TextRect.GetComponent<TextMeshProUGUI>().text = inputField.text;
             Area.NameText.text = "player";
+            
             inputField.interactable = false;
-
+            inputField.text = "답변을 기다리는 중입니다...";
+            sendBtn.interactable = false;
+            scrollBar.value = 0;
             //테스트용 코드 : 다른 상대와 대화 초기화하고 대화 시작
             if (TestMode && transferNum == 0) transferNum = -1;
             
@@ -60,22 +66,27 @@ public class SendMessage : MonoBehaviour
     private void NPCMessage(){
         Debug.Log(transferNum + 1 + "번째 메세지 수신");
         GameObject TextClone = Instantiate(_chatManager.OrangeArea, ContentRect);
-
         AreaScript Area = TextClone.GetComponent<AreaScript>();
-        
+        inputField.text = "";
         // NPC message -> text
         Area.TextRect.GetComponent<TextMeshProUGUI>().text = UserChatAPIManager.Instance.ResponseMessageInfo["response"];
 
         // NPC message -> name
         Area.NameText.text = targetPersonaName;
+        
+        //scrollview
+        scrollBar.value = 0;
     }
     
     public IEnumerator showNPCMessageCoroutine()
     {
+        yield return new WaitForSeconds(0.1f);
+        scrollBar.value = 0;
         while (true)
         {
             if(!UserChatAPIManager.Instance.isResponded)
             {
+
                 //메세지 수신 기다리는 중
                 yield return new WaitForSeconds(0.5f);
             }
@@ -84,12 +95,20 @@ public class SendMessage : MonoBehaviour
                 // NPc Message get
                 NPCMessage();
                 inputField.interactable = true;
+                sendBtn.interactable = true;
+              
+                if (transferNum == maxTransferNum)
+                {
+                    inputField.text = "더 이상 메세지를 전송할 수 없습니다.";
+                    inputField.interactable = false;
+                    sendBtn.interactable = false;
+                }
                 break;
-             
-
             } 
         }
-        yield return 0;
+        yield return new WaitForSeconds(0.2f);
+        
+        scrollBar.value = 0;
     }
     
 }
