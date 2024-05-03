a8f9d87e8d3d2d8d410df3f8f3a24e4a76187e4e
wwonnn <dnjsgml0508@naver.com>
2024-05-03 10:25:08 +0900

    connect ending

---
+++ Assets/02.Scripts/Event/Ending.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Ending : MonoBehaviour
+{
+    public GameObject endingCollider;
+    public TextMeshProUGUI text;
+    private int assembled_num;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        assembled_num = 0;
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(NPCServer.Clock.Instance.GetCurrentTime());
+        DateTime dateTime = new DateTime(DateTime.Now.Year, 8, 1, 8, 30, 0);
+        Debug.Log(dateTime);
+
+        // 종교 집회 시작
+        if(DateTime.Compare(NPCServer.Clock.Instance.GetCurrentTime(), dateTime) > 0)
+        {
+            Debug.Log("종교 집회 갑시다");
+            endingCollider.SetActive(true);
+
+            // NPC 종교 집회로 location 설정
+            GameObject[] NPC;
+            NPC = GameObject.FindGameObjectsWithTag("NPC");
+            foreach (GameObject npc in NPC)
+            {
+                int rIndex = ReligiousIndexNetworkManager.Instance.RIndexInfo[npc.name];
+                if(rIndex >= 30)
+                {
+                    npc.GetComponent<NPC>().AddWaypoint(GameManager.Instance.location[13], 1000);
+                }
+            }
+
+            assembled_num = gameObject.GetComponentInChildren<EndingCollider>().assembled_num;
+            text.text = "나주교의 종교 집회: " + assembled_num + "명";
+
+            // 테스트용 10초 뒤 엔딩
+            timer += Time.deltaTime;
+            if(timer > 10)
+            {
+                SceneManager.LoadScene("SuccessEvent");
+            }
+        }
+
+    }
+}
