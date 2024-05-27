5c42d51dbdcbf6576259312b0a8ab9aea810b237
wwonnn <dnjsgml0508@naver.com>
2024-05-27 23:03:17 +0900

    quiz

--- Assets/02.Scripts/_JW/DIALOGUE/SendMessage.cs
+++ Assets/02.Scripts/_JW/DIALOGUE/SendMessage.cs
@@ -207,9 +207,10 @@ public class SendMessage : MonoBehaviour
 
                     if (transferNum == maxTransferNum)
                     {
-  
-                        StartCoroutine("StartNPCQuiz");
-
+                        if (targetPersonaName != "나주교" && !GameObject.Find(targetPersonaName).GetComponent<NPCQuiz>().quizEnd)
+                        {
+                            StartCoroutine("StartNPCQuiz");
+                        }
                     }
                 }
                 break;
