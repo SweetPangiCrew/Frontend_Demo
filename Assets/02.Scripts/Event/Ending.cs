2f0841514496915ac177a2187041d7e39a9d99ca
dasoya <66422476+dasoya@users.noreply.github.com>
2024-05-29 00:01:00 +0900

    Update Ending.cs

--- Assets/02.Scripts/Event/Ending.cs
+++ Assets/02.Scripts/Event/Ending.cs
@@ -26,7 +26,7 @@ public class Ending : MonoBehaviour
     void Update()
     {
         //Debug.Log(NPCServer.Clock.Instance.GetCurrentTime());
-        DateTime dateTime = new DateTime(DateTime.Now.Year, 8, 1, 9, 0, 0);
+        DateTime dateTime = new DateTime(DateTime.Now.Year, 8, 1,23, 0, 0);
         //Debug.Log(dateTime);
 
         // 종교 집회 시작
@@ -62,17 +62,17 @@ public class Ending : MonoBehaviour
             panel.GetComponentInChildren<TextMeshProUGUI>().text = "현재 나주교의 종교 집회: " + assembled_num + "명 참석";
 
             // 엔딩
-            DateTime endingTime = new DateTime(DateTime.Now.Year, 8,1,12,30,0);
+            DateTime endingTime = new DateTime(DateTime.Now.Year, 8,2,0,30,0);
             if (DateTime.Compare(NPCServer.Clock.Instance.GetCurrentTime(), endingTime) > 0)
             {
                 Debug.Log("종교 집회 시작");
                 if(assembled_num < 7)
                 {
-                    //SceneManager.LoadScene("SuccessEvent");
+                    SceneManager.LoadScene("SuccessEvent");
                 }
                 else
                 {
-                    //SceneManager.LoadScene("FailEvent");
+                    SceneManager.LoadScene("FailEvent");
                 }
                 
             }
